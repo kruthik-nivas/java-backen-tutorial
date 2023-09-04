f7d792eb75fd4fe341490b73e765f5f13d98779b
Kruthik Nivas <kruthiknivasoff@gmail.com>
2023-09-04 10:23:29 +0530

    Simple calculator final code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,75 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        while (true) {
+            System.out.print("Enter the first value: ");
+            int number1 = scanner.nextInt();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+            System.out.print("Enter the second value: ");
+            int number2 = scanner.nextInt();
 
+            System.out.println("Press 1 for addition");
+            System.out.println("Press 2 for subtraction");
+            System.out.println("Press 3 for multiplication");
+            System.out.println("Press 4 for Division");
+            System.out.print("Enter your choice: ");
 
+            int choice = scanner.nextInt();
 
+            int result=0;
+            boolean defaultSelected = false;
+
+            switch (choice) {
+                case 1 -> result = addTwoNumbers(number1, number2);
+                case 2 -> result = subtractTwoNumbers(number1, number2);
+                case 3 -> result = multiplyTwoNumbers(number1, number2);
+                case 4 -> result = divideTwoNumbers(number1, number2);
+                default -> {
+                    System.out.println("Kindly Enter a valid option. Restarting the application");
+                    defaultSelected = true;
+                }
+            }
+
+            if(defaultSelected) {
+                continue;
+            }else {
+                System.out.println("The answer is: " + result);
+            }
+
+            System.out.println("Do you want to continue : ");
+            System.out.print("Enter [y/n]: ");
+
+            String option = scanner.next();
+
+            if(option.equals("n") || option.equals("N")){
+                break;
+            }else if(option.equals("y") || option.equals("Y")){
+                continue;
+            }else{
+                System.out.println("You have entered an invalid option, so quitting the application");
+                break;
+            }
         }
+
+    }
+
+    private static int addTwoNumbers(int number1,int number2){
+        return number1 + number2;
+    }
+    private static int subtractTwoNumbers(int number1,int number2){
+        return number1 - number2;
+    }
+    private static int multiplyTwoNumbers(int number1,int number2){
+        return number1 * number2;
+    }
+    private static int divideTwoNumbers(int number1,int number2){
+        return number1 / number2;
     }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Student {
+    public String name;
+    public int rollNumber;
+    public int age;
+}
